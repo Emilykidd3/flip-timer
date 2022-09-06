7a45c6027658c9399b28dbdeb7d499f3ea0288a4
emilykidd3 <e.kidd61@yahoo.com>
2022-09-05 21:12:20 -0700

    moved javascript into a function removed unnecessary variables

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-const startNumber = 5
 const flipCard = document.querySelector(".flip-card")
-const topHalf = flipCard.querySelector(".top")
-const bottomHalf = flipCard.querySelector(".bottom")
-const topFlip = document.createElement('div')
-topFlip.classList.add('top-flip')
-const bottomFlip = document.createElement('div')
-bottomFlip.classList.add('bottom-flip')
 
-topHalf.textContent = startNumber
-bottomHalf.textContent = startNumber
-topFlip.textContent = startNumber
-bottomFlip.textContent = startNumber - 1
-
-topFlip.addEventListener("animationstart", e => {
-    topHalf.textContent = startNumber - 1
-})
-topFlip.addEventListener("animationend", e => {
-    topFlip.remove()
-})
-bottomFlip.addEventListener("animationend", e => {
-    bottomHalf.textContent = startNumber - 1
-    bottomFlip.remove()
-})
-flipCard.append(topFlip, bottomFlip);
+flip(flipCard)
 
 function flip(flipCard) {
     const topHalf = flipCard.querySelector(".top")
@@ -31,4 +9,23 @@ function flip(flipCard) {
     topFlip.classList.add('top-flip')
     const bottomFlip = document.createElement('div')
     bottomFlip.classList.add('bottom-flip')
+    const startNumber = parseInt(topHalf.textContent)
+
+    topHalf.textContent = startNumber
+    bottomHalf.textContent = startNumber
+    topFlip.textContent = startNumber
+    bottomFlip.textContent = startNumber - 1
+
+    topFlip.addEventListener("animationstart", e => {
+        topHalf.textContent = startNumber - 1
+    })
+    topFlip.addEventListener("animationend", e => {
+        topFlip.remove()
+    })
+    bottomFlip.addEventListener("animationend", e => {
+        bottomHalf.textContent = startNumber - 1
+        bottomFlip.remove()
+        flip(flipCard)
+    })
+    flipCard.append(topFlip, bottomFlip);
 }
